7a3d86ef6cbd9d6e745dd42b4bb4a114698125e7
Yash <Prajapatiyash0011@gmail.com>
2024-06-04 23:15:08 +0530

    Initial commit

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -4,61 +4,65 @@ import 'package:chatter/services/auth_service.dart';
 import 'package:chatter/services/chat/chat_service.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+
 class ChatPage extends StatefulWidget {
   final String receiveremail;
   final String receiverID;
 
-  ChatPage({super.key,
-    required this.receiveremail,
-    required this.receiverID
-  });
+  ChatPage({super.key, required this.receiveremail, required this.receiverID});
 
   @override
   State<ChatPage> createState() => _ChatPageState();
 }
 
 class _ChatPageState extends State<ChatPage> {
-  //text controller
+  // Text controller
   final TextEditingController _messageController = TextEditingController();
 
-  //chat & auth services
+  // Chat & auth services
   final ChatService _chatService = ChatService();
-
   final AuthService _authService = AuthService();
 
   FocusNode myFocusNode = FocusNode();
+
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     myFocusNode.addListener(() {
-      if(myFocusNode.hasFocus){
-        Future.delayed(Duration(milliseconds: 500), ()=> scrollDown(),
+      if (myFocusNode.hasFocus) {
+        Future.delayed(
+          Duration(milliseconds: 500),
+              () => scrollDown(),
         );
       }
     });
-    Future.delayed(Duration(milliseconds: 500),
-        () => scrollDown(),
+    Future.delayed(
+      Duration(milliseconds: 500),
+          () => scrollDown(),
     );
   }
+
   @override
   void dispose() {
-    // TODO: implement dispose
     myFocusNode.dispose();
     _messageController.dispose();
     super.dispose();
   }
 
   final ScrollController _scrollController = ScrollController();
-  void scrollDown(){
-    _scrollController.animateTo(_scrollController.position.maxScrollExtent, duration: Duration(milliseconds: 300), curve: Curves.fastOutSlowIn);
+
+  void scrollDown() {
+    _scrollController.animateTo(
+      _scrollController.position.maxScrollExtent,
+      duration: Duration(milliseconds: 300),
+      curve: Curves.fastOutSlowIn,
+    );
   }
 
-  //send message
-  void sendMessage() async{
-    //if there is something to send
-    if(_messageController.text.isNotEmpty){
-      //send msg
+  // Send message
+  void sendMessage() async {
+    if (_messageController.text.isNotEmpty) {
+      // Send message
       await _chatService.sendMessage(widget.receiverID, _messageController.text);
       _messageController.clear();
     }
@@ -76,32 +80,33 @@ class _ChatPageState extends State<ChatPage> {
       ),
       body: Column(
         children: [
-          //display all messages
-          Expanded(child: _buildMessageList(),
+          // Display all messages
+          Expanded(
+            child: _buildMessageList(),
           ),
 
-          //user input
+          // User input
           _buildUserInput(),
         ],
       ),
     );
   }
 
-  //build mesg list
+  // Build message list
   Widget _buildMessageList() {
     String senderID = _authService.getCurrentUser()!.uid;
     return StreamBuilder(
       stream: _chatService.getMessages(widget.receiverID, senderID),
       builder: (context, snapshot) {
-        //errors check
+        // Error check
         if (snapshot.hasError) {
           return const Text("Error");
         }
-        //loading
+        // Loading
         if (snapshot.connectionState == ConnectionState.waiting) {
           return const Text("Loading..");
         }
-        //return list view
+        // Return list view
         return ListView(
           controller: _scrollController,
           children: snapshot.data!.docs.map((doc) => _buildMessageItem(doc)).toList(),
@@ -110,41 +115,52 @@ class _ChatPageState extends State<ChatPage> {
     );
   }
 
-  //build message item
-  Widget _buildMessageItem(DocumentSnapshot doc){
+  // Build message item
+  Widget _buildMessageItem(DocumentSnapshot doc) {
     Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
     bool isCurrentUser = data['senderID'] == _authService.getCurrentUser()!.uid;
-    var alignment = isCurrentUser ? Alignment.centerRight:Alignment.centerLeft;
+    var alignment = isCurrentUser ? Alignment.centerRight : Alignment.centerLeft;
+
     return Container(
       alignment: alignment,
-        child: Column(
-          crossAxisAlignment: isCurrentUser ? CrossAxisAlignment.end : CrossAxisAlignment.start,
-          children: [
-            ChatBubble(isCurrentUser: isCurrentUser, message: data["message"])
-          ],
-        ));
+      padding: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+      child: Column(
+        crossAxisAlignment: isCurrentUser ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+        children: [
+          ChatBubble(isCurrentUser: isCurrentUser, message: data["message"]),
+        ],
+      ),
+    );
   }
 
-  //build message input
-  Widget _buildUserInput(){
-    return  Padding(
+  // Build message input
+  Widget _buildUserInput() {
+    return Padding(
       padding: const EdgeInsets.only(bottom: 30),
       child: Row(
         children: [
-          Expanded(child: MyTextField(
-            controller: _messageController,
-            hintText: "Type a message", tf: false,
-            focusNode: myFocusNode,
-          ),
+          Expanded(
+            child: MyTextField(
+              controller: _messageController,
+              hintText: "Type a message",
+              tf: false,
+              focusNode: myFocusNode,
+            ),
           ),
           Container(
             decoration: BoxDecoration(
               color: Colors.green,
-              shape: BoxShape.circle
+              shape: BoxShape.circle,
             ),
-              margin: EdgeInsets.only(right: 25),
-              child: IconButton(onPressed: sendMessage, icon: Icon(Icons.arrow_upward,
-              color: Colors.white,)))
+            margin: EdgeInsets.only(right: 25),
+            child: IconButton(
+              onPressed: sendMessage,
+              icon: Icon(
+                Icons.arrow_upward,
+                color: Colors.white,
+              ),
+            ),
+          ),
         ],
       ),
     );
